808cfab4033c906c0aff2ee5786ad277dbe9a6b6
Rossi <mtrossi@labs.neit.edu>
2018-04-02 09:14:34 -0400

    Database Conn File

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php 
+
+$dsn = "mysql:host=localhost;dbname=PHPClassSpring2018";
+$userName = "PHPClassSpring2018";
+$pWord = "SE266";
+
+try{
+	
+	$db = new PDO($dsn, $userName, $pWord);
+	
+	
+	
+} catch(PDOException $e){
+	
+	die("Cannot Connect to the Database");	
+}
+
+?>
